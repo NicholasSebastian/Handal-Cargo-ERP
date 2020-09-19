d10a552741fd9202fc5e3dd9dedf6fb22450a1a4
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2020-09-19 12:05:46 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// https://www.javatpoint.com/java-jdbc
+// https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-usagenotes-connect-drivermanager.html#connector-j-examples-connection-drivermanager
+// https://www.youtube.com/playlist?list=PLsyeobzWxl7rU7Jz3zDRpqB-EODzBbHOI
+
+public class Main {
+	
+	static final String 
+	    hostname = "192.168.0.1", 
+	    port = "3306",	// Default value. Can be changed in the my.ini file.
+	    databaseName = "handalcargo",
+	    user = "handalcargo",
+	    password = "hunter1389";
+
+	public static void main(String[] args) {
+		// Initialize connection with database.
+		Database.loadDriver();
+		Database.loadConnection(hostname, port, databaseName, user, password);
+		
+		// Application runs here.
+		UserInterface app = new UserInterface();
+		app.setVisible(true);
+		
+		// Close the connection.
+		Database.closeConnection();
+	}
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+// Good luck ifat :)))
+
+public class UserInterface extends JFrame {
+	
+	public UserInterface() {
+		super("Handal Cargo - Integrated System");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 500);
+	}
+}
