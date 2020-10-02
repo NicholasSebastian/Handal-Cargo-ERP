df26170cbf6784d8fab7fa4f6c8b03618aed6314
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2020-10-02 23:35:02 +0700

    number fields only accept numbers now

--- src/UI/Components/NumberField.java
+++ src/UI/Components/NumberField.java
@@ -1,11 +1,23 @@
 package UI.Components;
 
+import java.awt.event.KeyEvent;
 import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class NumberField extends JTextField {
 
-	public NumberField() {
-		
+	@Override
+	public void processKeyEvent(KeyEvent e) {
+		if (Character.isDigit(e.getKeyChar()) || // numbers
+			e.getKeyChar() == '.' ||	// periods
+			e.getKeyCode() == 8 || 		// backspaces
+			e.getKeyCode() == 127 ||	// delete
+			e.getKeyCode() == 37 ||		// left arrow
+			e.getKeyCode() == 39		// right arrow
+		) {
+			super.processKeyEvent(e);
+		}
+		e.consume();
+		return;
 	}
 }
